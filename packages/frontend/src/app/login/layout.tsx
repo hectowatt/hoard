d19a6a71754444ff37737a1fbe093fa2fa10269a
hectowatt <w477.dev@gmail.com>
2026-01-25 02:36:30 +0000

    ビルドエラーが出ていたのでログイン画面のlayoutをサーバコンポーネントに変更

--- packages/frontend/src/app/login/layout.tsx
+++ packages/frontend/src/app/login/layout.tsx
@@ -1,15 +1,12 @@
-import { Box, CssBaseline } from '@mui/material';
 import { Viewport } from 'next';
 import React from 'react';
-
-const backgroundColor = '#e3a838';
 
 export const viewport: Viewport = {
     minimumScale: 1,
     initialScale: 1,
     width: 'device-width',
     viewportFit: 'cover',
-    themeColor: '#e3a838',
+    themeColor: 'theme.palette.primary.main'
 };
 
 export default function LoginLayout({
@@ -19,22 +16,7 @@ export default function LoginLayout({
 }) {
     return (
         <>
-            <CssBaseline />
-            <Box
-                component="main"
-                sx={{
-                    minHeight: '100dvh',
-                    pt: 'env(safe-area-inset-top)',
-                    pb: 'env(safe-area-inset-bottom)',
-                    bgcolor: backgroundColor,
-                    width: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                {children}
-            </Box>
+            {children}
         </>
     );
 }
